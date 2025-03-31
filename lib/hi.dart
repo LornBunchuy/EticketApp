1d717317b874e46628c8687a5333939b2f343c6d
bunchuy <bunchuygaming@gmail.com>
2025-03-31 20:34:53 +0700

    add test

---
+++ lib/hi.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyHello extends StatefulWidget {
+  const MyHello({super.key});
+
+  @override
+  State<MyHello> createState() => _MyHelloState();
+}
+
+class _MyHelloState extends State<MyHello> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
